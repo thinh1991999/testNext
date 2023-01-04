35bc961d79fba63d13ecb1cae901488e8af67fee
thinh <thinhtyt1999@gmail.com>
2023-01-04 16:45:44 +0700

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {},
